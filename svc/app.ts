75662473ee2f3478ee29a72ead12c0edbc386962
Shininess Engraving <shininessengraving852@example.com>
2025-03-18 19:22:11 -0700

    start serving on /

--- svc/app.ts
+++ svc/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+import { Request, Response } from 'express';
+
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
 app.listen(3000, () => {
 	  console.log('Server is running on port 3000');
